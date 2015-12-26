c9a4839888326a82096e868b425dd8a47897027e
Sherman Hewitt <shewitt7@umiami.edu>
2015-12-26 16:02:55 -0500

    Started server code

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Server alive!");
+var mongo = require("mongodb").MongoClient,
+    client = require("socket.io").listen(8080).sockets;
+
+client.on("connection", function(socket) {
+  console.log("Test cxn");
+});
+
+// https://youtu.be/GkXQEwHn2nA?t=6m54s
